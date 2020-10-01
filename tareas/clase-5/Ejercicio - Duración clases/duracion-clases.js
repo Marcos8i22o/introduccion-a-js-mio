403a381e7d35828a71855b78ff33ae17306ffd2c
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-01 12:21:37 -0300

    agrega-funcion-guardarClasesElegidas

--- tareas/clase-5/Ejercicio - Duración clases/duracion-clases.js
+++ tareas/clase-5/Ejercicio - Duración clases/duracion-clases.js
@@ -1,52 +1,65 @@
 const $botonCalcular = document.querySelector("#calcular");
-const $botonCantidadDeClases = document.querySelector('#enviar');
-
+const $botonCantidadDeClases = document.querySelector("#enviar");
 
 $botonCantidadDeClases.onclick = function () {
-
   const casillerosClases = [];
 
-  let numeroClase = '';
-  almacenarCasillerosClases(casillerosClases,numeroClase);
+  let numeroClase = "";
+  almacenarCasillerosClases(casillerosClases, numeroClase);
+  guardarClasesElegidas(casillerosClases);
   habilitarClase(numeroClase);
-}
+};
 
 $botonCalcular.onclick = function () {
-  
   return false;
 };
 
-
-function habilitarClase (numeroClase){
-
+function habilitarClase(numeroClase) {
   const horas = document.querySelector(`#hora-clase-${numeroClase}`);
   const minutos = document.querySelector(`#minutos-clase-${numeroClase}`);
   const segundos = document.querySelector(`#segundos-clase-${numeroClase}`);
-  
+
   horas.disabled = false;
   minutos.disabled = false;
   segundos.disabled = false;
-  
 }
 
-function almacenarCasillerosClases (casillerosClases,numeroClase) {
-  
- 
+function almacenarCasillerosClases(casillerosClases) {
   for (let i = 1; i <= 5; i++) {
-    casillerosClases.push(document.querySelector(`input[name=clase${i}]`).checked);
+    casillerosClases.push(
+      document.querySelector(`input[name=clase${i}]`).checked
+    );
   }
-
 }
 
-
-//   for (let i = 1; i <= casillerosClases.length; i++) {
-    
-//     if (casillerosClases[i]) {
-//       numeroClase = casillerosClases[i+1].toString();
-//       console.log(numeroClase)
-//     }
-//   }
-
+function guardarClasesElegidas(casillerosClases) {
   
+  const clasesElegidas = [];
 
-// }
+  for (let i = 0; i < casillerosClases.length; i++) {
+    if (casillerosClases[i]) {
+      switch (i) {
+        case 0:
+          clasesElegidas.push("uno");
+          break;
+        case 1:
+          clasesElegidas.push("dos");
+          break;
+        case 2:
+          clasesElegidas.push("tres");
+          break;
+        case 3:
+          clasesElegidas.push("cuatro");
+          break;
+        case 4:
+          clasesElegidas.push("cinco");
+          break;
+        default:
+          clasesElegidas.push("");
+          break;
+      }
+      
+    }
+  }
+  return clasesElegidas;
+}
